100f7d13d1b7e1700382e5c4ff94d3045b68702e
shivamp20 <shivampandey.pandey8@gmail.com>
2022-01-17 00:21:52 +0530

    commit after DB connection

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ const app = express()
 const port = 3000
 const static = express.static("public");
 
+//Firebase credentials and connection
+var admin = require("firebase-admin");
+var serviceAccount = require("./urlshotener-b9a2a-firebase-adminsdk-lv8nt-fc507a0dcd.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const urlsdb = admin.firestore().collection("urlsdb");
 app.use(static);
 
 //app.use((req,res,next) => {
   //  console.log('We interceted your request');
     //next();
 //})
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get("/:short",(req,res) => {
+    console.log(req.params);
+    const short = req.params.short
+
+    const doc = urlsdb.doc(short);
+    doc.get().then(response =>{
+      const data = response.data();
+      if(data && data.url){
+        res.redirect(301,data.url);
+      } else {
+        res.redirect(301,"https://www.pipsnacks.com/404");
+      }
+    })
+    //res.send('We will redirect you to ' +short);
 })
 
-app.get('/another',(req,res) => {
-    res.send('hello world from another');
+app.post("/admin/urls/",(req,res) => {
+  console.log(req.body);
 })
 
 app.listen(port, () => {
